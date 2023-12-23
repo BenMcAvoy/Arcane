7f0db57b0b84b3e69e45aee456840d70e0e989a7
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-23 01:57:33 +0000

    Setup ECS with Shipyard

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use macroquad::{prelude::*, main};
+use macroquad::{main, prelude::*};
 use minifemme::*;
+use shipyard::*;
+
+mod ecs;
+use ecs::*;
 
 #[main("Arcane")]
 async fn main() {
     minifemme::start(LevelFilter::Info, LogMode::Pretty);
 
+    let mut world = World::default();
+
+    world.add_entity(Position {
+        x: 128.0,
+        y: 128.0
+    });
+
     loop {
         clear_background(BLACK);
+
+        world.run(|positions: View<Position>| {
+            for position in positions.iter() {
+                draw_rectangle(position.x, position.y, 50., 50., WHITE);
+            }
+        });
 
         next_frame().await
     }
